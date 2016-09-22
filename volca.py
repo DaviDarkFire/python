095593da509b83cd2800ea713807d693f32cbf1b
DaviDarkFire <daviferreirasantog@gmail.com>
2016-09-22 14:40:27 -0400

    acho que ta funfando

--- volca.py
+++ volca.py
@@ -1,16 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- 
 
-def slide(W,w,w_mod,flag):			#o parâmetro flag indica se eu vou ter que preencher o w completamente, ou se vou precisar apenas adicionar um dia e excluir o primeiro
-	if flag:						#se a flag == 1 significa que eu tenho que preencher toda janela w
-		keys_W = W.keys()			#faz uma lista com as chaves do W
-		for i in keys_W[:w_mod+1]:	#preenche  a janela w com os valores de W
-			w[i] = W[i]		
-	else:
-		keys_w = w.keys()			#se a flag for 0, somente elimina-se a primeira posição e adciona-se uma nova chave no final da janela
-		del w[keys_w[0]]
-		w[keys_w[-1]+1] = W[keys_w[-1]+1]
-
+def slide(W,w,w_mod):			#o parâmetro flag indica se eu vou ter que preencher o w completamente, ou se vou precisar apenas adicionar um dia e excluir o primeiro	
+	keys_W = W.keys()			#faz uma lista com as chaves do W
+	for i in keys_W[:w_mod+1]:	#preenche  a janela w com os valores de W
+		w[i] = W[i]			
 	return w    					#retorna a janela atualizada
 
 
@@ -40,7 +34,7 @@ def iprice_min(w):				#função que retorna o endereço/dia do menor ponto
 f = open("input.in", "r") #abre o arquivo de entradas
 w_mod = int(f.readline()) #le o |w|
 p = int(f.readline())     #le o p
-W = {}                    #cria o dicionario W
+W = {1:1,2:20,3:-10,4:40,5:-50,6:60,7:70,8:80,9:-90,10:100}                    #cria o dicionario W
 w = {}					  #cria o dicionário w	
 v_max = {}                #cria o dicionario v_max
 v_min = {}                #cria o dicionario v_min
@@ -55,11 +49,11 @@ for i in W:						#inicializa os dicionários v_max, v_min, v_class
 for linha in f:					#inicializa W com os valores de entrada
     pass
 
-slide(W,w,w_mod,1)
-for i in W:											#laço que vai levando a janela w e votando nos pontos de máximo e mínimo, alterar
+keys_W = W.keys()
+for i in keys_W[:len(W)-w_mod]:											#laço que vai levando a janela w e votando nos pontos de máximo e mínimo, alterar
+	slide(W,w,w_mod)
 	v_max[iprice_max(w)] = v_max[iprice_max(w)]+1
-	v_min[iprice_min(w)] = v_min[iprice_min(w)]+1
-	slide(W,w,w_mod,0)
+	v_min[iprice_min(w)] = v_min[iprice_min(w)]+1	
 
 
 for i in v_max:					                    #preenche o dicionário v_class com as informações de compra e venda
@@ -67,6 +61,9 @@ for i in v_max:					                    #preenche o dicionário v_class com as i
         v_class[i] = "sell"
     if v_min[i] > limit:
         v_class[i] = "buy"
+
+for i in v_class:	
+	print v_class[i]
 
 
 f.close()                                          #fecha o arquivo de entrada
